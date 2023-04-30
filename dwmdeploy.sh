57842a9ffb01cbc50e0eac2ac350f1e844bee8d0
Sean-IT <128379384+SeanIT05@users.noreply.github.com>
2023-04-30 12:43:22 +0000

    Add files via upload

--- dwmdeploy.sh
+++ dwmdeploy.sh
@@ -12,23 +12,43 @@ sleep 3
 
 # Shameless plug
 echo -e "Have a look at \e[0;35mhttps://github.com/SeanIT05/ \e[0m\n"
-sleep 2
+echo -e "\e[0;35mUsing default make.conf \e[0m"
+echo -e "\e[0;31mPlease adjust gentoo/portage/make.conf to your liking! \e[0m\n"
+echo "LTO, PGO, Graphite is also an option (advanced)"
+echo "If interested use gentoo/portage/make.conf.lto instead \n"
 
+# Copying default make.conf
 echo -e "\e[0;36mCopying /etc/portage/make.conf! \e[0m"
-sleep 1
+sleep 10
+
 sudo cp gentoo/portage/make.conf /etc/portage/make.conf
 
-echo -e "\e[0;36mPreparing your system! \e[0m"
+echo -e "\e[0;36mCompiling software... \e[0m"
 
 # Requires packages
-sudo emerge dev-vcs/git libXft libXinerama xorg-server && sudo mkdir /home/$USER && sudo cp -a config /home/$USER
+sudo emerge dev-vcs/git libXft libXinerama xorg-server rust-bin alacritty picom feh && sudo mkdir /home/$USER .config && sudo cp -a config /home/$USER
 
 # Compiling DWM
+echo -e "\e[0;36mFinished compiling! \e[0m\n"
+sleep 3
+echo -e "\e[0;36mCompiling DWM\e[0m"
 cd .config/dwm && sudo make clean install
 cd .config/slstatus && sudo make clean install
 cd .config/dmenu && sudo make clean install
 
-echo -e "Have a look at \e[0;36mBuilt DWM and configuration copied! \e[0m"
-echo -e "Have a look at \e[0;36mEmerging alacritty \e[0m"
-sleep 0.3
-sudo emerge alacritty
+# After DWM Compile
+echo -e "\e[0;36mBuilt DWM and configuration copied! \e[0m"
+sleep 2
+
+# Copying xinitrc
+sudo cp config/xinitrc /home/$USER .xinitrc
+sudo cp bg /usr/share/bg
+
+# Updating System
+echo "Do you wish to update your system? [Y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        sudo emerge --newuse --update --deep @world
+else
+        echo "Skipping..."
+fi
